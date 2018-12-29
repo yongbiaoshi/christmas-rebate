8ba57d4b5d42695ec70c15496892fa824836c5bf
shiyongbiao@incar100.com <shiyongbiao@incar100.com>
2018-12-29 14:42:23 +0800

    '添加手动发起发奖功能'

--- src/main/java/com/ground/christmas/rebate/controller/HomeController.java
+++ src/main/java/com/ground/christmas/rebate/controller/HomeController.java
@@ -2,17 +2,22 @@ package com.ground.christmas.rebate.controller;
 
 import com.ground.christmas.rebate.entity.RebateInfo;
 import com.ground.christmas.rebate.service.ChristmasRebateService;
+import com.ground.christmas.rebate.service.DingTalkRobotService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 import java.util.List;
 
+@Slf4j
 @RestController
 public class HomeController {
 
     @Resource
     ChristmasRebateService service;
+    @Resource
+    DingTalkRobotService dingTalkRobotService;
 
     @GetMapping("")
     public String index() {
@@ -21,7 +26,15 @@ public class HomeController {
 
     @GetMapping("rebate")
     public List<Long> rebate() {
-        return service.rebate();
+
+        log.info("圣诞活动发奖手动发奖开始");
+        // 发奖
+        List<Long> uids = service.rebate();
+        // 通知发短信
+        dingTalkRobotService.sendText("圣诞活动短信用户ID：" + uids.toString(), true);
+        log.info("圣诞活动发奖手动发奖结束");
+
+        return uids;
     }
 
     @GetMapping("find")
